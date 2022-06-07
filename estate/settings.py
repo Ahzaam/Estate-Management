4254838b81e877b9fd0b76382eae6349f4b19c7d
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-07 21:36:05 +0530

    auth google

--- estate/settings.py
+++ estate/settings.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = ['estatemanagements.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'service.apps.ServiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,6 +130,29 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 #
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
