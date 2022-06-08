de1fffff56c48ee77ce9148f550f8d8a0eb53349
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-09 01:13:04 +0530

    nothing

--- estate/urls.py
+++ estate/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'GLiDE Ceylon admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
